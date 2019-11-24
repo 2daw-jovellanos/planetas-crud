5fab30f60abfa50c952f8696a6015853eb3e0cf3
Víctor J. Fernández <victor.fernandezgil@educa.madrid.org>
2019-11-24 04:54:39 +0100

    Error 500

--- index.php
+++ index.php
@@ -1,34 +1,39 @@
 <?php
-$router=true;
+$router = true;
 require_once "cargarconfig.php";
 require_once "controladores/PlanetasController.php";
+require_once "modelo/OrmException.php";
 $controller = $_REQUEST["controller"] ?? "listado";
-
-switch ($controller) {
-    case "nuevo":
-        if ($_SERVER["REQUEST_METHOD"]=="POST") {
-            (new PlanetasController)->recogerInsertar();
-        } else {
-            (new PlanetasController)->insertar();
-        }
-        break;
-    case "modificacion":
-            if ($_SERVER["REQUEST_METHOD"]=="POST") {
+try {
+    switch ($controller) {
+        case "nuevo":
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                (new PlanetasController)->recogerInsertar();
+            } else {
+                (new PlanetasController)->insertar();
+            }
+            break;
+        case "modificacion":
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 (new PlanetasController)->recogerModificacion();
             } else {
                 (new PlanetasController)->modificar();
             }
-            break;        
-    case "listado":
-        (new PlanetasController)->listado();
-        break;
-    case "borrado":
-        (new PlanetasController)->confirmarBorrado();
-        break;
-    case "borrado-efectivo":
+            break;
+        case "listado":
+            (new PlanetasController)->listado();
+            break;
+        case "borrado":
+            (new PlanetasController)->confirmarBorrado();
+            break;
+        case "borrado-efectivo":
             (new PlanetasController)->borradoEfectivo();
             break;
-    default:
-        die("controlador solicitado desconocido");
-        break;
+        default:
+            die("controlador solicitado desconocido");
+            break;
+    }
+} catch (OrmException $ex) {
+    http_response_code(500);
+    require "vistas/500.phtml";
 }
